2615d2a3209e2236840384c48fa1b9b60e49e1de
liuxiyu <xiyuliu509@gmail.com>
2024-06-22 13:01:31 +0800

    6.22：13

--- js/user/login.js
+++ js/user/login.js
@@ -4,16 +4,20 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     const userPassword = document.getElementById('userPassword').value;
     axios.post('http://localhost:8082/user/login', { userName, userPassword })
         .then(response => {
-            if (response.data) {
-                alert('登录成功, ' + response.data.userName);
+            if (response.data.isAdmin) {
+                alert('管理员登录成功, ' + response.data.userName);
                 alert('Login time: ' + response.data.loginTime); // 在登录成功的弹窗中显示登录时间
                 localStorage.setItem('user', JSON.stringify(response.data));
                 window.location.href = '/html/user/user.html';
             } else {
-                alert('凭据无效，请重试.');
+                alert('用户登录成功, ' + response.data.userName);
+                alert('Login time: ' + response.data.loginTime); // 在登录成功的弹窗中显示登录时间
+                localStorage.setItem('user', JSON.stringify(response.data));
+                window.location.href = '/html/user/user.html';
             }
         })
         .catch(error => {
+            alert('凭据无效，请重试.');
             console.error(error);
         });
 });
--- js/user/register.js
+++ js/user/register.js
@@ -4,15 +4,18 @@ document.getElementById('registerForm').addEventListener('submit', function (e) 
     const userPassword = document.getElementById('userPassword').value;
     const userPhone = document.getElementById('userPhone').value;
     const userWallet = document.getElementById('userWallet').value;
-    axios.post('http://localhost:8082/user/create', { userName, userPassword, userPhone, userWallet })
+    axios.post('http://localhost:8082/user/create', { userName, userPassword, userPhone, userWallet, isAdmin: false }) 
         .then(response => {
-            // 处理注册成功的逻辑
             alert('注册成功！');
             window.location.href = '/html/user/login.html'; // 注册成功后跳转到登录页面
         })
         .catch(error => {
-            console.error(error);
-            alert('注册失败，请重试。');
+            if (error.response && error.response.data) {
+                alert(error.response.data); // Display more specific error message from the server
+            } else {
+                console.error(error);
+                alert('注册失败，请重试。');
+            }
         });
 });
 
